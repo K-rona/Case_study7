e66e351cb32238853a97fc6c2b68c3e9b700ebe9
1Kuznetsov <i.kuznetsov5@g.nsu.ru>
2024-02-21 19:46:52 +0700

    Localization

--- Case.py
+++ Case.py
@@ -1,3 +1,6 @@
+import ru_local as ru
+
+
 fuel_amount = {}
 lost_clients = 0
 
@@ -12,8 +15,15 @@ with open('automats.txt', encoding='utf8') as f_auto:
             if item not in fuel_amount:
                 fuel_amount[item] = 0
 
+n = len(dict_queue)
+
 
 with open('input.txt', encoding='utf8') as f_in:
     for new_client in f_in:
         time, volume, brand = new_client.split()
-        fuel_amount[brand] += volume
+        fuel_amount[brand] += int(volume)
+
+        print(f'{ru.IN} {time} {ru.NEW_CLIENT}: {new_client[:-1]} {ru.GET_IN_LINE}')
+        for i in range(n):
+            print(f'{ru.AUTOMAT}{i+1} {ru.MAX_QUEUE}{dict_queue[str(n)][0]} {ru.GASOLINE_BRANDS} {dict_queue[str(n)][1]}->')
+
