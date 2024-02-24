0d92ebc3668d04407542fcc3d924e0b36d31e3d8
TC <yakimova.tonya@gmal.com>
2024-02-25 05:03:53 +0700

    25.02

--- Case_Test.py
+++ Case_Test.py
@@ -1,14 +1,15 @@
-import Ru_local as ru
+import ru_local as ru
 import random
 
 
 fuel_amount = {}
 clients = []
 lost_clients = 0
+total_time = 0
 
 brands = ['АИ-80', 'АИ-92', 'АИ-95', 'АИ-98']
 fill_amount = random.randint(3, 7)
-with open('automats.txt', 'w') as f_write:
+with open('automats.txt','w') as f_write:
     ptr = ''
     for i in range(1, fill_amount + 1):
         line = str(i) + ' ' + str(random.randint(1, 4)) + ' '
@@ -25,24 +26,59 @@ with open('automats.txt', 'r') as f_auto:
     dict_queue = {}
     for ptr in f_auto:
         data = ptr.split()
-        dict_queue[data[0]] = [(data[1]), data[2:], []]
+        dict_queue[data[0]] = [(data[1]), data[2:], [],[]]
 
         for item in data[2:]:
             if item not in fuel_amount:
                 fuel_amount[item] = 0
 
+def status_now(dict_queue,total_time):
+    for i in range(1, fill_amount + 1):
+        clients = []
+        filled = []
+        for j in range(len(dict_queue[str(i)][3])):
+
+            if len(dict_queue[str(i)][3][j])>0:
+                time_end = dict_queue[str(i)][3][j][4] + dict_queue[str(i)][3][j][3]
+
+                if time_end < total_time:
+                    hours = str(time_end // 60)
+                    minuts = str(time_end - int(hours) * 60)
+                    if len(hours)<2:
+                        hours = "0" + hours
+                    if len(minuts)<2:
+                        minuts = "0" + minuts
+                    '''print(f'{ru.IN} {hours}:{minuts} {ru.CLIENT} '
+                          f'{dict_queue[str(i)][3][j][0]} {dict_queue[str(i)][3][j][2]} {dict_queue[str(i)][3][j][1]} {ru.FILL_LUCK}')'''
+                    print("fjkfkfkfkfkfk")
+                else:
+                    clients.append(dict_queue[str(i)][3][j])
+                    filled.append(dict_queue[str(i)][2][j])
+        dict_queue[str(i)][3] = clients
+        dict_queue[str(i)][2] = filled
+
+
+
+
+
 
 n = len(dict_queue)
 with open('input.txt', encoding='utf8') as f_in:
     for new_client in f_in:
+        #clients = []
         time, volume, brand = new_client.split()
-        fuel_amount[brand] += int(volume)
+        time_in_minuts = int(time[:2]) * 60 + int(time[3:])
+        total_time += time_in_minuts - total_time
+        if brand in fuel_amount:
+            fuel_amount[brand] += int(volume)
 
         filling_time = round(int(volume) / 10)
         case = random.randint(-1, 1)
         if filling_time + case > 0:
             filling_time += case
-            clients += [time, volume, brand, filling_time]
+            clients = [time, volume, brand, filling_time, time_in_minuts]
+
+        status_now(dict_queue, total_time)
 
         min_que = float('inf')
         for i in range(1, fill_amount + 1):
@@ -51,7 +87,7 @@ with open('input.txt', encoding='utf8') as f_in:
                     min_que = len(dict_queue[str(i)][2])
 
         if min_que == float('inf'):
-            print(f'{ru.IN} {time} {ru.NEW_CLIENT}: {new_client} {filling_time} {ru.FILL_FAIL}')
+            print(f'{ru.IN} {time} {ru.NEW_CLIENT}: {new_client[:-1]} {filling_time} {ru.FILL_FAIL}')
             lost_clients += 1
         else:
             for j in range(1, fill_amount + 1):
@@ -59,7 +95,8 @@ with open('input.txt', encoding='utf8') as f_in:
                     if len(dict_queue[str(j)][2]) == min_que and len(dict_queue[str(j)][2]) < int(dict_queue[str(j)][0]):
                         fill_number = j
                         dict_queue[str(j)][2] += ['*']
-                        print(f'{ru.IN} {time} {ru.NEW_CLIENT}: {new_client} {filling_time} {ru.GET_IN_LINE} {fill_number}')
+                        dict_queue[str(j)][3] += [clients]
+                        print(f'{ru.IN} {time} {ru.NEW_CLIENT}: {new_client[:-1]} {filling_time} {ru.GET_IN_LINE} {fill_number}')
                         break
 
         for k in range(1, fill_amount + 1):
